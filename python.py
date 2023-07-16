7679cdfa58d92184f2d556e7a7161a9620ae14f7
olaab24-meet <139681340+olaab24-meet@users.noreply.github.com>
2023-07-17 01:18:31 +0300

    Update python.py

--- python.py
+++ python.py
@@ -3,12 +3,20 @@ print("rawr")
 def create_youtube_video(title,description):
 	meow={"title":title,"description":description,"likes":0,"dislikes":0,"comments":{"username":""}}
 	return meow
-
+meow=create_youtube_video("onwan","wasf")
 def dislike(dictionary):
-	if "dislikes" in dictionary==True:
+	if "dislikes" in dictionary:
 		dictionary["dislikes"]=dictionary["dislikes"]+1
 	return dictionary
 def like(dictionary):
-	if "likes" in dictionary==True:
+	if "likes" in dictionary:
 		dictionary["likes"]=dictionary["likes"]+1
 	return dictionary
+def add_comment(dictionary,username,comment_text):
+	dictionary["comments"]["useername"]=comment_text
+	return dictionary
+print("now for some fun hehehe")
+for i in range(500):
+	meow=like(meow)
+print(meow["likes"])
+print(add_comment(meow,"wawa","skibidi toilet"))
